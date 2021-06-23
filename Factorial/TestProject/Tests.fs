9889e04675cd683e75bb6ab5a88a6289627fa185
Alexander <AlexSmith@live.dk>
2021-06-23 14:18:06 +0200

    update

--- Factorial/TestProject/Tests.fs
+++ Factorial/TestProject/Tests.fs
@@ -1,13 +1,9 @@
 module Tests
-#load "/Users/alexandermolholm/Documents/IKT/6. Semester/AFP/ExamExer/Factorial/Factorial/Factorial.fsproj"
 
 open Factorial
 open Xunit
 
-
-
-
 [<Fact>]
 let ``Testing factoral`` () =
     let answer = fact(5,1)
-    Assert.True(true)
+    Assert.Equal(answer,  120)
